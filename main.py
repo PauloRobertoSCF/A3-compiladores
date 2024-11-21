eb90c881ae38fe2af4df2fecbf58b052a5c90bd9
gyodanbr <148167151+gyodanbr@users.noreply.github.com>
2024-11-20 22:01:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
+import sys
+from lexer import lexer
 from parser import parser
-
-# Exemplo de código para testar
-data = '''
-programa
-    inteiro x;
-    decimal y;
-    x := 10;
-    y := x + 3.5;
-    if (x > y) escreva y;
-    fimprog
-'''
-
-# Executa o parser no código de teste
-parser.parse(data)
- 
+from semantic import SemanticAnalyzer, SemanticError
+from generator import CodeGenerator
