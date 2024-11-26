af9bfd0535cd7f3155fe1884cb4882df7f670ac8
gyodanbr <148167151+gyodanbr@users.noreply.github.com>
2024-11-25 22:14:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from lexer import lexer
 from parser import parser
 from semantic import SemanticAnalyzer, SemanticError
-from generator import CodeGenerator
+from code_generator import CodeGenerator
 from ast import
 
 def main():
